9aa86852f2a6d4a670b292fd673d187443900376
Paulo Roberto Dias <paulo.roberto@startcorp.com.br>
2026-05-18 15:42:53 -0300

    feat: adicionar imagem de capacete e plantas na seção de serviços

--- src/App.tsx
+++ src/App.tsx
@@ -223,7 +223,7 @@ export default function App() {
               <div className="relative mt-8 lg:mt-0">
                 <div className={`aspect-[4/5] rounded-sm overflow-hidden border shadow-2xl ${darkMode ? 'border-white/5' : 'border-black/5'}`}>
                   <img
-                    src="https://images.unsplash.com/photo-1504307651254-35680f356dfd?auto=format&fit=crop&q=80&w=1200"
+                    src="/public/mdr-img.jpeg"
                     alt="Capacete e Plantas"
                     className={`w-full h-full object-cover transition-all duration-700 ${darkMode ? 'grayscale hover:grayscale-0' : 'hover:scale-105'}`}
                     referrerPolicy="no-referrer"
